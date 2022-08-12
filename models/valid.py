f67b48de75b70ceede7b843887c88fc5cc1e90e3
shinning0821 <92084444+shinning0821@users.noreply.github.com>
2022-08-12 16:19:40 +0800

    Update valid.py

--- models/valid.py
+++ models/valid.py
@@ -23,6 +23,7 @@ from monai.transforms import (
     EnsureTyped,
     EnsureType,
 )
+import torchmetrics
 
 post_pred = Compose(
     [EnsureType(), Activations(softmax=True), AsDiscrete(threshold=0.5)]
@@ -57,4 +58,35 @@ def compute_DiceMetric(args,device,val_loader,model):
     # aggregate the final mean dice result
     metric = dice_metric.aggregate().item()
     dice_metric.reset()
-    return metric
+    return metric,val_images,val_labels,val_outputs
+
+
+
+def compute_F1Metric(args,device,val_loader,model):
+    f1_metric = torchmetrics.F1(
+            num_classes=args.num_class, average='macro', ignore_index=0, mdmc_average='global', multiclass=True)
+    val_images = None
+    val_labels = None
+    val_outputs = None
+    for val_data in val_loader:
+        val_images, val_labels = val_data["img"].to(device), val_data[
+            "label"
+        ].to(device)
+        val_labels_onehot = monai.networks.one_hot(
+            val_labels, args.num_class
+        )
+        roi_size = (256, 256)
+        sw_batch_size = 4
+        val_outputs = sliding_window_inference(
+            val_images, roi_size, sw_batch_size, model
+        )
+        val_outputs = [post_pred(i) for i in decollate_batch(val_outputs)]
+        val_labels_onehot = [
+            post_gt(i) for i in decollate_batch(val_labels_onehot)
+        ]
+        # compute F1metric for current iteration
+        f1_metric(val_outputs,val_labels_onehot[:])
+    # aggregate the final mean dice result
+    metric = f1_metric.compute()
+    f1_metric.reset()
+    return metric,metric,val_images,val_labels,val_outputs
